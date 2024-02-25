1d1d5f5e0612b6a8fcbb4da5c61fd7249c4a4692
= <=>
2024-02-25 21:35:00 +0100

    Primer commit Sesion5

---
+++ operations.js
@@ -0,0 +1,16 @@
+/* Función suma */
+var suma = function suma (a, b) {
+	return a + b;	
+};
+
+/* Función resta */
+var resta = function suma (a, b) {
+	return a - b;	
+};
+
+/* Exportación de funciones */
+module.exports = {
+	suma,
+	resta
+}
+
